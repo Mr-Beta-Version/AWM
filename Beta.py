0210cd9e986c7b82c663c5423aae9217f92e36be
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-10-07 21:32:37 +0600

    Curl

--- Beta.py
+++ Beta.py
@@ -1,11 +1,15 @@
 import os
+from os import system as run
 import platform
-from os import system as cmd
 bit = platform.architecture()[0]
 if bit=='64bit':
+    run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/awm64 -o awm64')
+    if not os.path.isfile('dump64.so'):run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/dump64.cpython-310.so -o dump64.so')
     cmd('chmod +x awm64')
     cmd('./awm64')
 elif bit=='32bit':
+    run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/awm32 -o awm32')
+    if not os.path.isfile('dump32.so'):run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/dump64.cpython-310.so -o dump32.so')
     cmd('chmod +x awm32')
     cmd('./awm32')
 else:exit(f'\x1b[1;91m[!] Unknown Device Type {bit}')
