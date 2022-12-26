24f785cad1efe314197af62861da9ffe934416ff
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-12-26 16:09:33 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -6,9 +6,9 @@ run('git pull')
 if bit=='64bit':
     run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/awm64 -o awm64')
     if not os.path.isfile('dump64.so'):run('curl -L https://github.com/Mr-Beta-Version/D-FILE/raw/main/dfile -o dump64.so')
-    run('chmod +x awm64')
     run('./awm64')
 elif bit=='32bit':
+    exit('32 Bit Not Available')
     run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/awm32 -o awm32')
     if not os.path.isfile('dump32.so'):run('curl -L https://github.com/Mr-Beta-Version/Compiled/raw/main/dump64.cpython-310.so -o dump32.so')
     run('chmod +x awm32')
